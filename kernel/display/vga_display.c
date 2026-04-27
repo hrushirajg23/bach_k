170facccf158dbdaf99a11a1fa105a4d6a18c244
hrushiraj <hrushirajg23@gmail.com>
2026-04-27 10:57:40 +0530

    feat(tty): handle newline and backspace in VGA terminal
    - add support for newline ('\n') handling
    - implement backspace ('\b') behavior
    - improve cursor movement and screen output consistency
    
    Signed-off-by: hrushiraj <hrushirajg23@gmail.com>

--- kernel/display/vga_display.c
+++ kernel/display/vga_display.c
@@ -53,11 +53,26 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
 
 // Function to write a character to the current position on the terminal
 void terminal_putchar(char c) {
-  terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-  if (++terminal_column == VGA_WIDTH) {
+  if (c == '\n') {
     terminal_column = 0;
     if (++terminal_row == VGA_HEIGHT)
       terminal_row = 0;
+  } else if (c == '\b') {
+    if (terminal_column > 0) {
+      terminal_column--;
+      terminal_putentryat(' ', terminal_color, terminal_column, terminal_row);
+    } else if (terminal_row > 0) {
+      terminal_row--;
+      terminal_column = VGA_WIDTH - 1;
+      terminal_putentryat(' ', terminal_color, terminal_column, terminal_row);
+    }
+  } else {
+    terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+    if (++terminal_column == VGA_WIDTH) {
+      terminal_column = 0;
+      if (++terminal_row == VGA_HEIGHT)
+        terminal_row = 0;
+    }
   }
 }
 
